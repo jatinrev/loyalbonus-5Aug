8c7c5eeefb56ec01c64fb107aa02833a79bdcf8d
pushkertiwari <pushker@revinfotech.com>
2016-09-06 17:46:16 +0530

    things done by pushker

--- www/components/myOrders/order_reciept.js
+++ www/components/myOrders/order_reciept.js
@@ -1,6 +1,6 @@
 angular.module('LoyalBonus')
 
-.controller('orderRecieptController', function ($scope, $state, ajaxCall, active_controller,$rootScope, refreshTest) {
+.controller('orderRecieptController', function ($scope, $state, ajaxCall, active_controller,$rootScope, refreshTest,popUp,$ionicModal) {
 
 	active_controller.set('orderRecieptController');
 	$scope.Test = function () {
@@ -39,5 +39,34 @@ angular.module('LoyalBonus')
     });
 
 
-
+    $ionicModal.fromTemplateUrl('components/myOrders/my-modal.html',{
+        scope:$scope,
+        animation:'slide-in-up'
+    }).then(function(modal){
+        $scope.modal = modal;
+    });
+    $scope.closeZoomView=function(){
+        console.log("cvhfghj");
+        $scope.modal.hide();
+    }
+    $scope.track_package=function(){
+        console.log("hjhf");
+        ajaxCall
+        .get('webapi/UserCartAPI/GetTrackingDetails', {
+            /*orderId : "11707D46-8448-4F92-9835-990D641E92AB",*/
+           /* orderId :"e5f5ab80-1197-4025-a0d9-7d9dcb3af40b",*/
+            UserId :$rootScope.userDetails.userId,
+            orderId :$state.params.order_id
+            /*userId:"428"*/
+        })
+        .then(function(res){
+            $scope.modal_data = res.data.Data
+            console.log($scope.modal_data)
+            $scope.modal.show();
+             /*$scope.modal.hide();*/
+           /* popUp
+            .msgPopUp("fdh");*/
+            console.log(res);
+        });        
+    }
 });
