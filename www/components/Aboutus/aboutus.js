99ad376c588c64388ba3549ddff957f9e1897d70
pushkertiwari <pushker@revinfotech.com>
2016-09-21 13:00:06 +0530

    changes in privacy by pushker

--- www/components/Aboutus/aboutus.js
+++ www/components/Aboutus/aboutus.js
@@ -1,8 +1,26 @@
 angular.module('LoyalBonus')
 
-.controller('AboutusController', function () {
+.controller('AboutusController', function ($scope,$state,refreshTest,$ionicModal,active_controller) {
 	
-/**/
+active_controller.set('AboutusController');
+	$scope.Test = function () {
+        return refreshTest.showrefreshtest($state.current.name, $state.params);
+}
+     $ionicModal.fromTemplateUrl('components/Aboutus/privacymodal.html',{
+        scope:$scope,
+        animation:'slide-in-up'
+    }).then(function(modal){
+        $scope.modal = modal;
+    });
+    $scope.closeZoomView=function(){
+ 
+        $scope.modal.hide();
+    }
+    $scope.privacy_up=function(){
+        $scope.modal.show();
+        
+    }
+           
 });
 
 
